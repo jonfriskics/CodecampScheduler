1a628f8b198557f51fb1e437fbb99746eedd34bc
Jon Friskics <jonfriskics@gmail.com>
2018-03-16 22:47:37 -0400

    add tasks route

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -6,6 +6,21 @@ public func routes(_ router: Router) throws {
         return "Hello, world!"
     }
 
+    router.get("talks") { req -> Future<View> in
+        struct TalksContext: Codable {
+            var talks: [Talk]
+        }
+
+        _ = Talk(id: 1, presenter: "Jon Friskics...").create(on: req)
+        _ = Talk(id: 2, presenter: "David Haney...").create(on: req)
+        _ = Talk(id: 3, presenter: "Devan Beitel...").create(on: req)
+
+        return Talk.query(on: req).all().flatMap(to: View.self) { talks in
+            let context = TalksContext(talks: talks)
+            return try req.view().render("talks", context)
+        }
+    }
+
     router.get("talk", Int.parameter) { req -> Future<View> in
         let id = try req.parameter(Int.self)
         
